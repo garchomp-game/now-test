b38dec778ddddb33660b3489f62d521a0cf3aff5
otoka 響風 <otoka.japanesedrum@gmail.com>
2019-05-22 17:17:19 +0900

    first-commit

---
+++ pages/hoge/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="">
+      <h1>about page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-btn to="hoge/about">about page</v-btn>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
